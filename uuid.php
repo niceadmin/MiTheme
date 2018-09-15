46a690f024041aa26a35552bef3bb0c02d11a360
niceadmin <38182065+niceadmin@users.noreply.github.com>
2018-09-15 12:25:21 +0800

    Create uuid.php

---
+++ uuid.php
@@ -0,0 +1,16 @@
+<?php
+/**
+  * Generates an UUID
+  */
+  function uuid()
+  {
+    $chars = md5(uniqid(mt_rand(), true));
+    $uuid  = substr($chars,0,8) . '-';
+    $uuid .= substr($chars,8,4) . '-';
+    $uuid .= substr($chars,12,4) . '-';
+    $uuid .= substr($chars,16,4) . '-';
+    $uuid .= substr($chars,20,12);
+    return $uuid;
+  }  
+
+?>
